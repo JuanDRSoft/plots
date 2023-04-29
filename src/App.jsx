996b353a17c80f3c2d04cd3dce225034f99b54dd
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2023-04-29 12:32:13 -0500

    feat: final

--- src/App.jsx
+++ src/App.jsx
@@ -33,56 +33,116 @@ function App() {
   };
 
   return (
-    <div className="bg-gray-100 w-full h-full">
-      <h1 className="flex justify-center pt-10 font-bold text-4xl bg-blue-700 pb-5 text-white shadow">
-        Plots List
-      </h1>
+    <div className="w-full h-full">
+      <img src="src/img/fondo.png" class="fixed -z-10 w-full h-screen top-0" />
+      <div className="flex">
+        <div className="bg-glass p-10 rounded-md shadow  w-1/2 h-screen flex items-center">
+          <div className="w-full justify-center">
+            <h1 className="text-7xl mb-24 font-semibold text-center">
+              Find Your Plot
+            </h1>
+            <div className="flex justify-center">
+              <input
+                type="number"
+                className="border-t border-l border-b rounded-l-md bg-gray-50 border-blue-700 outline-none pl-3"
+                onChange={(e) => setSearch(e.target.value)}
+                value={search}
+                placeholder="Enter your Plot No."
+              />
 
-      <div className="p-10">
-        <div className="flex bg-white p-5 rounded-md shadow">
-          <label className="w-1/4 font-semibold">Enter the plot number</label>
-          <input
-            type="text"
-            className="w-full border rounded-md bg-gray-50"
-            onChange={(e) => setSearch(e.target.value)}
-            value={search}
-          />
+              <select
+                onChange={(e) => setSelect(e.target.value)}
+                value={select}
+                className="bg-gray-50 border-blue-700 border-t border-l border-b"
+              >
+                <option>Residential Plots</option>
+                <option>Commercial Plots</option>
+              </select>
+              <select
+                onChange={(e) => setSelectBlock(e.target.value)}
+                value={selectBlock}
+                className="bg-gray-50 border-blue-700 border"
+              >
+                {block.map((e) => (
+                  <option>{e}</option>
+                ))}
+              </select>
 
-          <button
-            className="bg-blue-700 text-white p-1 ml-5 rounded-md mr-5"
-            onClick={onSearch}
-          >
-            Search
-          </button>
-
-          <select onChange={(e) => setSelect(e.target.value)} value={select}>
-            <option>Residential Plots</option>
-            <option>Commercial Plots</option>
-          </select>
-
-          <select
-            onChange={(e) => setSelectBlock(e.target.value)}
-            value={selectBlock}
-          >
-            {block.map((e) => (
-              <option>{e}</option>
-            ))}
-          </select>
+              <button
+                className="bg-blue-700 text-white p-3 rounded-r-md w-full lg:w-1/4 font-bold hover:bg-blue-800 duration-500 flex items-center gap-2 justify-center"
+                onClick={onSearch}
+              >
+                SEARCH
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
         </div>
 
-        <div className="mt-5">
+        <div className="mt-5 w-1/2 p-10 flex items-center">
           {plot.map((e) => (
-            <div className="bg-white mb-2 rounded-md shadow p-5 flex justify-between">
-              <h1>Plot No. {e.plotNo}</h1>
-              <p>{e.block}</p>
-              <p>Size: {e.size}</p>
-              <p>Status: {e.status}</p>
-              <p>Corner: {e.corner}</p>
-              <p>Extra Land: {e.extraland}</p>
-              <p>Marlas: {e.marlas}</p>
-              <p>Main Double Rain: {e.mainDoubleRoad}</p>
-              <p>filling: {e.filling}</p>
-              <p>Street No:{e.streetNo}</p>
+            <div className="bg-white mb-2 rounded-md shadow-lg p-5 justify-between w-full">
+              <div className="flex justify-between items-baseline">
+                <h1 className="text-3xl font-bold">Plot No. {e.plotNo}</h1>
+                <p className="font-semibold text-xl">
+                  Block:{" "}
+                  <span className="font-normal text-blue-700">{e.block}</span>
+                </p>
+              </div>
+
+              <div className="grid grid-cols-2 mt-5 mb-5">
+                <p className="font-semibold mt-5 text-lg">
+                  Size:{" "}
+                  <span className="font-normal text-blue-700">{e.size}</span>
+                </p>
+                <p className="font-semibold mt-5 text-lg">
+                  Status:{" "}
+                  <span className="font-normal text-blue-700">{e.status}</span>
+                </p>
+                <p className="font-semibold mt-5 text-lg">
+                  Corner:{" "}
+                  <span className="font-normal text-blue-700">{e.corner}</span>
+                </p>
+                <p className="font-semibold mt-5 text-lg">
+                  Extra Land:{" "}
+                  <span className="font-normal text-blue-700">
+                    {e.extraLand}
+                  </span>
+                </p>
+                <p className="font-semibold mt-5 text-lg">
+                  Marlas:{" "}
+                  <span className="font-normal text-blue-700">{e.marlas}</span>
+                </p>
+                <p className="font-semibold mt-5 text-lg">
+                  Main Double Rain:{" "}
+                  <span className="font-normal text-blue-700">
+                    {e.marlasDoubleRain}
+                  </span>
+                </p>
+                <p className="font-semibold mt-5 text-lg">
+                  filling:{" "}
+                  <span className="font-normal text-blue-700">{e.filling}</span>
+                </p>
+                <p className="font-semibold mt-5 text-lg">
+                  Street No:{" "}
+                  <span className="font-normal text-blue-700">
+                    {e.streetNo}
+                  </span>
+                </p>
+              </div>
             </div>
           ))}
         </div>
